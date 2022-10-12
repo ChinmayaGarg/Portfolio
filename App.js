f4bfc3a06c7a25782f8fcbaf15d879fca37ccd97
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-12 20:21:46 +0530

    Hid the progress bar and displayed the button after last gif ends

--- App.js
+++ App.js
@@ -34,3 +34,9 @@ const disp = i => {
 };
 
 disp(1);
+
+setTimeout(function () {
+  document.getElementsByClassName('progressBar')[0].style.display = 'none';
+  document.getElementsByClassName('enterBtn')[0].style.display = 'initial';
+  disp(i + 1);
+}, 9100);
