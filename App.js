a05de79e1f7116590708c7b5e7b8384135007cca
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-10-12 20:10:42 +0530

    Progress Bar Styled and Added

--- App.js
+++ App.js
@@ -5,18 +5,23 @@ const disp = i => {
     switch (i) {
       case 1:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#f4aa3c';
+        document.getElementsByClassName('progressBarFull')[0].style.backgroundColor = '#e93040';
         break;
       case 2:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#fdfdfd';
+        document.getElementsByClassName('progressBarFull')[0].style.backgroundColor = '#7b7071';
         break;
       case 3:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#84bd00';
+        document.getElementsByClassName('progressBarFull')[0].style.backgroundColor = '#1679b9';
         break;
       case 4:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#f96b8d';
+        document.getElementsByClassName('progressBarFull')[0].style.backgroundColor = 'gray';
         break;
       case 5:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#f0f0f0';
+        document.getElementsByClassName('progressBarFull')[0].style.backgroundColor = '#000';
         break;
       default:
         document.getElementsByClassName('loaderWrapper')[0].style.backgroundColor = '#fff';
